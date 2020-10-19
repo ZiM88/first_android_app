a67328419ddf63d4dce746c5a59e468ca0f588f4
ZiM88 <renegadez@tut.by>
2020-10-19 10:29:20 +0300

    android app

---
+++ app/src/main/java/com/android/uraall/pizzarecipes/RecipeActivity.java
@@ -0,0 +1,24 @@
+package com.android.uraall.pizzarecipes;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class RecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+
+        TextView title = findViewById(R.id.titleTextView);
+        TextView recipe = findViewById(R.id.recipeTextView);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            title.setText(intent.getStringExtra("title"));
+            recipe.setText(intent.getStringExtra("recipe"));
+        }
+    }
+}
